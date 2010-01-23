7cde4ea7cc913e1623a4cfd772975bc056937b37
dorfy@clanplastic.com <dorfy@clanplastic.com@d9684588-0613-11df-8268-c7ed62af1467>
2010-01-23 00:01:41 +0000

    Edited Hitman to sit with citizen

--- trunk/DarkRP/gamemode/shared.lua
+++ trunk/DarkRP/gamemode/shared.lua
@@ -32,7 +32,7 @@ TEAM_CITIZEN = AddExtraTeam("Citizen", Color(20, 150, 20, 255), "models/player/g
 besides being a hobo. 
 You have no specific role in city life.]], {}, "citizen", 0, 45, 0, false, false)
 
-TEAM_UNDERCOVERCOP = AddExtraTeam("UnderCover Cop", Color(20, 150, 20, 255), "models/player/group01/male_01.mdl", [[Fight the law from the underground.
+TEAM_UNDERCOVERCOP = AddExtraTeam("CUnderCover Cop", Color(20, 150, 20, 255), "models/player/group01/male_01.mdl", [[Fight the law from the underground.
 ]], {"arrest_stick", "unarrest_stick", "weapon_real_cs_glock18", "stunstick", "door_ram", "weaponchecker", "item_ammo_pistol"}, "undercovercop", 2, 50, 0, false, false, TEAM_POLICE)
 
 TEAM_HOBO = AddExtraTeam("Hobo", Color(80, 45, 0, 255), "models/player/corpse1.mdl", [[The lowest member of society. All people see you laugh. 
@@ -122,7 +122,7 @@ It is not illegal to strip in a bar.
 TEAM_ADMIN = AddExtraTeam("Admin", Color(255, 0, 0, 255), "models/player/combine_super_soldier.mdl", [[Admin.
 Enough said.]], {""}, "admin", 1, 100, 3, false, true)
 
-TEAM_HIT = AddExtraTeam("Hitman", Color(20, 150, 20, 255), "models/player/group01/male_01.mdl", [[Kill people for money.
+TEAM_HIT = AddExtraTeam("CHitman", Color(20, 150, 20, 255), "models/player/group01/male_01.mdl", [[Kill people for money.
 It is aganst the law to murder, you will be arrested.
 Only kill people if someone pays you to kill them. Or else its RDM]], {"weapon_real_cs_knife"}, "hitman", 1, 50, 0, false, false)
 
