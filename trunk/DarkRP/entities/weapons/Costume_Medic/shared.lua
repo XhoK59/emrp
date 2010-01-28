7df5ebe55efa52d5b2245a1b7647e4f3cbfcd0c5
dorfy@clanplastic.com <dorfy@clanplastic.com@d9684588-0613-11df-8268-c7ed62af1467>
2010-01-28 00:47:40 +0000

    --Added Costumes

---
+++ trunk/DarkRP/entities/weapons/Costume_Medic/shared.lua
@@ -0,0 +1,77 @@
+if( SERVER ) then
+	AddCSLuaFile( "shared.lua" );
+end
+
+if( CLIENT ) then
+
+	SWEP.PrintName = "Medic Costume";
+	SWEP.Slot = 3;
+	SWEP.SlotPos = 1;
+	SWEP.DrawAmmo = false;
+	SWEP.DrawCrosshair = false;
+
+end
+
+// Variables that are used on both client and server
+
+SWEP.Author			= "Epinephrine"
+SWEP.Instructions	= "Left Click To Dress As A Medic. Right Click To Change Back."
+SWEP.Category		= "JRRP Costumes"
+SWEP.Contact		= ""
+SWEP.Purpose		= ""
+
+SWEP.ViewModelFOV	= 60
+SWEP.ViewModelFlip	= false
+
+SWEP.Spawnable			= false
+SWEP.AdminSpawnable		= true
+
+SWEP.NextStrike = 0;
+  
+SWEP.ViewModel = Model( "models/weapons/v_c4.mdl" );
+SWEP.WorldModel = Model( "models/weapons/w_c4.mdl" );
+  
+local ShootSound = Sound( "weapons/ar_reload.wav" );
+  
+SWEP.Primary.ClipSize		= -1					// Size of a clip
+SWEP.Primary.DefaultClip	= 0				// Default number of bullets in a clip
+SWEP.Primary.Automatic		= false				// Automatic/Semi Auto
+SWEP.Primary.Ammo			= ""
+
+SWEP.Secondary.ClipSize		= -1			// Size of a clip
+SWEP.Secondary.DefaultClip	= 0				// Default number of bullets in a clip
+SWEP.Secondary.Automatic	= false				// Automatic/Semi Auto
+SWEP.Secondary.Ammo			= ""
+SWEP.Spawnable			= false
+SWEP.AdminSpawnable		= true
+SWEP.ActivateTime = 0
+SWEP.CountTime = 0
+SWEP.HUDTime = 0
+
+/*---------------------------------------------------------
+	Name: Equip
+	Desc: A player or NPC has picked the weapon up
+//-------------------------------------------------------*/ 
+function SWEP:Equip( NewOwner )
+local gw = self.Owner:GetModel()
+	self.Owner:SetNWString("OriginalModel", gw)
+end
+/*---------------------------------------------------------
+   Name: SWEP:PrimaryAttack( )
+   Desc: +attack1 has been pressed
+---------------------------------------------------------*/
+function SWEP:PrimaryAttack()
+	self.BaseClass.ShootEffects( self );
+	self.Weapon:EmitSound (ShootSound); 
+	self.Owner:SetModel("models/player/kleiner.mdl")
+end
+ 
+/*---------------------------------------------------------
+  SecondaryAttack
+  ---------------------------------------------------------*/
+function SWEP:SecondaryAttack()
+local originalmodel = self.Owner:GetNWString("OriginalModel")
+	self.Weapon:EmitSound( ShootSound );
+	self.BaseClass.ShootEffects( self )
+	self.Owner:SetModel( originalmodel )
+end
