9c82b4bc569969bb29cef9b407aa3df88d7931b8
dorfy@clanplastic.com <dorfy@clanplastic.com@d9684588-0613-11df-8268-c7ed62af1467>
2010-01-30 05:49:59 +0000

    We can now see who kills who.

--- DarkRP/gamemode/player.lua
+++ DarkRP/gamemode/player.lua
@@ -574,3 +574,14 @@ end
 hook.Add("PlayerDeath", "HitDeath", HitDeath)
 
 
+function whokilledwho(victim,inflictor,killer)
+for k,v in pairs(player.GetAll()) do
+if v:IsAdmin() then
+if victim:IsValid() and killer:IsValid() and victim:IsPlayer() and killer:IsPlayer() then
+v:PrintMessage(HUD_PRINTTALK,victim:Nick().." was killed by "..killer:Nick().."!")
+end
+end
+end
+end
+hook.Add("PlayerDeath","informTheAdmins",whokilledwho)
+
